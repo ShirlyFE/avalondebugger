c1a964ff05e419371047e8308a6723da14ee6c0e
shirlyLoveU <tianxuelihello@sina.cn>
2015-03-30 17:31:52 +0800

    add avalon icon

--- panel/app.js
+++ panel/app.js
@@ -12,7 +12,7 @@
                 })
             }
         }
-
+    bglog(chrome.devtools.panels)
     var appVM = avalon.define('app', function(vm) {
         vm.$skipArray = ['vmid']
         vm.debugMode = false
